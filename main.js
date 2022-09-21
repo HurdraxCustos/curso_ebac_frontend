56d882b30ce19fbe59757579a63d94d9e7147aec
Ivan Soares Correia <108367711+HurdraxCustos@users.noreply.github.com>
2022-09-20 21:46:00 -0300

    Exercício - Módulo 7

---
+++ main.js
@@ -0,0 +1,18 @@
+
+let botaoValidar = document.getElementById('btnvalidar').onclick;
+
+btnvalidar.addEventListener('click', function() {
+    let primeiroCampo = parseInt(document.getElementById("campoA").value);
+    let segundoCampo = parseInt(document.getElementById("campoB").value);
+    
+    if (primeiroCampo === segundoCampo) {
+        alert("Inválido! O Campo A é igual ao Campo B!");
+    }
+    if (primeiroCampo > segundoCampo) {
+        alert("Inválido! O Campo A é maior que o Campo B!");
+    }
+
+    if (primeiroCampo < segundoCampo) {
+        alert("Válido! O Campo B é maior que o Campo A!");
+    }
+});
